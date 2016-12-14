a51fb04eb7b6c250bfd1fa2613b4b6fadb874729
Pixel Expanse <tim@pixelexpanse.com>
2016-12-14 13:35:56 -0500

    First Upload

---
+++ _vti_cnf/animate.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|webmaster
+vti_modifiedby:SR|webmaster
+vti_timelastmodified:TR|25 Dec 2007 22:21:33 -0000
+vti_timecreated:TR|25 Dec 2007 22:21:33 -0000
+vti_cacheddtm:TX|25 Dec 2007 22:21:33 -0000
+vti_filesize:IR|14261
+vti_extenderversion:SR|5.0.2.2634
+vti_backlinkinfo:VX|index2.htm
